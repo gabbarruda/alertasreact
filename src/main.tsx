e7b52d192a8efb5989e729f53ba4c147d2db2f05
Gabriela Arruda <gabrielacosta460@hotmail.com>
2023-09-15 21:26:46 -0300

    foto

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Header from './components/Header'
 import MenuLateral from './components/MenuLateral'
-import Alertas from './pages/Alertas'
 
 
 
 
 import 'react-calendar/dist/Calendar.css';
-import Teste from './pages/Teste'
-import Painel from './pages/Painel'
+import Alertas from './pages/Alertas'
 // import Alerta from './pages/Alerta'
 
 
@@ -28,9 +26,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <Routes>   
     <Route path='alertas' element={<Alertas/>}/>
-    {/* <Route path='teste' element={<Alerta/>}/> */}
-    <Route path='teste' element={<Teste/>}/>
-    <Route path='painel' element={<Painel/>}/>
 
 
       </Routes>
--- src/pages/Alertas/index.tsx
+++ src/pages/Alertas/index.tsx
@@ -1,25 +1,84 @@
 import Calendar from "react-calendar";
+
 import "./style.css"
 import 'react-calendar/dist/Calendar.css';
-import { Link } from "react-router-dom";
+// import Login from './pages/Login'
+// import Home from './pages/Home'
+
+// import Grafico from "../../assets/img/grafico-painel.png"
+import Resultado from "../../assets/img/demonstrativo.png"
+import Painel from "../../assets/img/grafico-painel.png"
 
 import { useState } from "react";
 
 
 function Alertas() {
+
+
+
   type ValuePiece = Date | null;
 
   type Value = ValuePiece | [ValuePiece, ValuePiece];
 
     const [value, onChange] = useState<Value>(new Date());
   
-    return (
-      <div className="testecalendario">
+    
+    
+  return (
+    <>
+      <section className="mainCards">
+        <div className="cards">
+          <div className="card critico">
+            <div className="card-conten">
+              <div className="number">50</div>
+              <div className="card-name">Alertas criticos</div>
+            </div>
+            <div className="icon-box">
+              <i className="fas fa-user-graduate" />
+            </div>
+          </div>
+          <div className="card urgentes">
+            <div className="card-conten">
+              <div className="number">30</div>
+              <div className="card-name">Urgentes</div>
+            </div>
+            <div className="icon-box">
+              <i className="fas fa-chalkboard-teacher" />
+            </div>
+          </div>
+          <div className="card normais">
+            <div className="card-conten">
+              <div className="number">20</div>
+              <div className="card-name">Normais</div>
+            </div>
+            <div className="icon-box">
+              <i className="fas fa-users" />
+            </div>
+          </div>
+          <div className="card concluido">
+            <div className="card-conten">
+              <div className="number">20</div>
+              <div className="card-name">Concluído</div>
+            </div>
+            <div className="icon-box">
+              <i className="fas fa-users" />
+            </div>
+          </div>
+        </div>
+        <div className="grafico">
+          <img src={ Resultado } alt="btn-user-info" />
+          <img src={ Painel } alt="btn-user-info" />
+
+        </div>
+        <div className="testecalendario">
         <Calendar onChange={onChange} value={value} />
         
       </div>
-      
-    );
+      </section>
+    </>
+
+
+  )
 }
 
-    export default Alertas
+export default Alertas
